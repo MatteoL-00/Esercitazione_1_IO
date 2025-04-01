e48965f8af58a1f91d661a5f0af5466cb1147152
MatteoL-00 <126798548+MatteoL-00@users.noreply.github.com>
2025-04-01 19:09:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,28 +15,35 @@ int main()
     unsigned int B=5;
     int a =-1;
     int b = 2;
-    double mean=0 ;
-    unsigned int n = 1;
+    unsigned int N = 1000;
     std::vector<double> values;
     double value;
     std::vector<double> means;
        while (inputFile >> value) {
 
-           value=(value - A)*(b-a)/(B-A) + a;  
+           value=(value - A)*(b-a)/(B-A) + a;
            values.push_back(value);
-           double somma=0;
-           for(double valore:values){
-               somma+=valore;}
-           double mean1= somma / n;
-           n++;
-           mean= mean1;
+           inputFile.close();
 
+
+
+          std::ofstream outputFile("result.txt");
+          if (outputFile.is_open()) {
+              double somma=0;
+
+                  for (unsigned int i = 0; i < N-1 ; i++) {
+                      somma += values[i];
+                      double media = somma / (i + 1);
+                      outputFile << i + 1 << " " << media << std::endl;
+
+                  }
+                  outputFile.close();
+
+            } else {
+                std::cout << "Errore: impossibile aprire il file." << std::endl;
+            }
 
    }
-
-   std::cout << "mean =   " << mean << std::endl;
-
-
 
     return 0;
 }
